255a60deb845d497ccf45e1949c4ca33ab0e5fcc
pusheando las notas del curso js <lautarodisalvo5@gmail.com>
2023-05-13 16:39:35 -0300

    callback hell

--- Mas temas/Callback.js
+++ Mas temas/Callback.js
@@ -1,30 +1,30 @@
 function contarCallback(value, cbk) {
-    setTimeout(() => {
-        cbk(value, value * value);
-    }, 0 | Math.random() * 1000);
+  setTimeout(() => {
+    cbk(value, value * value);
+  }, 0 | (Math.random() * 1000));
 }
 
 contarCallback(0, (value, result) => {
-    console.log("inicia callback");
+  console.log("inicia callback");
+  console.log(`Callback: ${value}, ${result}`);
+  contarCallback(1, (value, result) => {
     console.log(`Callback: ${value}, ${result}`);
-    contarCallback(1, (value, result) => {
-        console.log(`Callback: ${value}, ${result}`);
-    })
     contarCallback(2, (value, result) => {
+      console.log(`Callback: ${value}, ${result}`);
+      contarCallback(3, (value, result) => {
         console.log(`Callback: ${value}, ${result}`);
-    })
-    contarCallback(3, (value, result) => {
-        console.log(`Callback: ${value}, ${result}`);
-    })
-    contarCallback(4, (value, result) => {
-        console.log(`Callback: ${value}, ${result}`);
-    })
-    contarCallback(5, (value, result) => {
-        console.log(`Callback: ${value}, ${result}`);
-    })
-    console.log("Termina callback");
-    contarCallback(6, (value, result) => {
-        console.log(`Callback: ${value}, ${result}`);
-    })
-});
+        contarCallback(4, (value, result) => {
+          console.log(`Callback: ${value}, ${result}`);
+          contarCallback(5, (value, result) => {
+            console.log(`Callback: ${value}, ${result}`);
+            contarCallback(6, (value, result) => {
+              console.log(`Callback: ${value}, ${result}`);
 
+              console.log("Termina callback");
+            });
+          });
+        });
+      });
+    });
+  });
+});
