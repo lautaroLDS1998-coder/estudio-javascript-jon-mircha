c17c32a86d5db1ae91140db1ab9fcb24f3831ea1
pusheando las notas del curso js <lautarodisalvo5@gmail.com>
2023-05-13 16:20:34 -0300

    agregando callback y recursividad

--- Asincronia/asincronia.js
+++ Asincronia/asincronia.js
@@ -9,7 +9,7 @@ setTimeout(()=> {
 //   console.log(new Date().toLocaleTimeString());
 }, 1000);
 
-// estos dos valores (setTimeout, setInterval) tienen sus contrapartes pero se activan
+// estos dos valores (setTimeout, setInterval) tienen sus contraparteshttps://github.com/lautaroLDS1998-coder/estudio-javascript-jon-mircha.git pero se activan
 // si están declarados dentro de una variable y retornas esa variable
 
 // ver en el curso de jon mircha o en mozilla dn
